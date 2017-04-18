5ddb665607fd17dd8f26a062d5358a352792c528
Vu Nguyen <tuanvu.vn007@gmail.com>
2017-04-18 22:19:07 +0700

    add react router 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Helmet } from 'react-helmet'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Nav from './components/nav'
+import HomePage from './pages/home'
+import AboutPage from './pages/about'
+import './index.css'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Helmet titleTemplate="React App | %s" defaultTitle="React App" />
+        <Nav />
+
+        <div className="main-content">
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
